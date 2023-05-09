e0b79dc14fb7d88783e7dbbcb6b75a35e8128e87
senugegee <senugege@gmail.com>
2023-05-09 07:55:54 +0200

    Ch
    chinese zodiac

---
+++ Ch.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Ch{
+	public static void main(String[] args){
+		int year, rest;
+		System.out.println("Dogum yili ? ");
+		Scanner inp = new Scanner(System.in);
+		year = inp.nextInt();
+		rest = year%12;
+		switch (rest){
+			case 0:
+				System.out.println("Maymun");
+				break;
+			case 1:
+				System.out.println("Horoz");
+				break;
+			case 2:
+				System.out.println("Kopek");
+				break;
+			case 3:
+				System.out.println("Domuz");
+				break;
+			case 4:
+				System.out.println("Fare");
+				break;
+			case 5:
+				System.out.println("Okuz");
+				break;
+			case 6:
+				System.out.println("Kaplan");
+				break;
+			case 7:
+				System.out.println("Tavsan");
+				break;
+			case 8:
+				System.out.println("Ejderha");
+				break;
+			case 9:
+				System.out.println("Yilan");
+				break;
+			case 10:
+				System.out.println("At");
+				break;
+			case 11:
+				System.out.println("Koyun");
+				break;
+			default:
+				System.out.println("Hata! Bir daha dene!");
+				break;
+		}
+	}
+}
