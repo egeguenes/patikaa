0f7329cb9444c50cae37034fb140edb51f995f56
senugegee <senugege@gmail.com>
2023-05-15 23:31:39 +0200

    Muk sayilar

--- Muk.java
+++ Muk.java
@@ -4,6 +4,7 @@ public class Muk{
 	public static void main(String[] args){
 		int zahl1, boler, result;
 		result =0;
+		System.out.println("Mukemmelligini test etmek istediginiz sayiyi girin.");
 		Scanner ant = new Scanner(System.in);
 		zahl1 = ant.nextInt();
 		for (int i=1; i<zahl1; i++){
@@ -11,9 +12,9 @@ public class Muk{
 				result += i;
 			}
 		} if (result==zahl1) {
-			System.out.println(zahl1 + " bir muk sayidir.");
+			System.out.println(zahl1 + " bir mukemmel sayidir.");
 		} else {
-			System.out.println(zahl1 + " bir muk sayi degildir.");
+			System.out.println(zahl1 + " bir mukemmel sayi degildir.");
 		}
 	}
 }
