9addb763698ede3ba572531ce8a508a98c602ed6
senugegee <132394731+senugegee@users.noreply.github.com>
2023-05-17 14:40:15 +0200

    BasTop.java

--- BasTop.java
+++ BasTop.java
@@ -4,7 +4,7 @@ public class BasTop{
 	public static void main(String[] args){
 		int num1, b, summe;
 		Scanner ant = new Scanner(System.in);
-		System.out.println("Geben Sie eine Nummer ein : ");
+		System.out.println("Geben Sie eine Zahl ein : ");
 		num1 = ant.nextInt();
 		summe = 0;
 		while(num1 != 0){
