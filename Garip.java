291cceced5229acbd79269a00703b78972265cfe
y0111930 <e.guenes@tu-braunschweig.de>
2023-05-22 21:47:31 +0200

    Yildiz

---
+++ Garip.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Garip{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Gib eine zahl ein : ");
+        int n = input.nextInt();
+        for (int i=1; i<=n; i++){
+            for (int k=1; k<=(n-i); k++){
+                System.out.print(" ");
+            } for (int j=1; j<=(2*i)-1; j++){
+               System.out.print("*");
+            } System.out.println();
+        }
+    }
+}
